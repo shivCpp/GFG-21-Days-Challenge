152f790e66f992b8299ba3f853a70f6ba25f834d
Shivverma <162031756+shivCpp@users.noreply.github.com>
2024-09-14 15:48:14 +0530

    Update Mirror_Tree.cpp

--- Mirror_Tree.cpp
+++ Mirror_Tree.cpp
@@ -29,3 +29,19 @@ class Solution {
     
         // code here
     }
+
+**************************************************************************************
+
+class Solution {
+  public:
+    void mirror(Node* node) {
+     if (node== NULL) {
+        return;
+    }
+     mirror(node-> left);
+    mirror(node-> right);
+    swap(node-> left , node-> right);
+    }
+};
+
+
